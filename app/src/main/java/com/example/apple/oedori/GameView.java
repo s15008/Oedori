5a4d9dae7de0d87c587bb4306f5524033d2995f6
Osamu Nakanishi <s15008@std.it-college.ac.jp>
2016-09-21 20:31:56 +0900

    create プロジェクトの作成

---
+++ app/src/main/java/com/example/apple/oedori/GameView.java
@@ -0,0 +1,14 @@
+package com.example.apple.oedori;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by apple on 2016/09/21.
+ */
+
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+}
